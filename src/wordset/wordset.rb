4d0d48277b7e997095e939537dce8fb3248632d4
KOSEKI Kengo <koseki@gmail.com>
2016-07-19 02:45:10 +0900

    Update raw wordset data generate script

--- src/wordset/wordset.rb
+++ src/wordset/wordset.rb
@@ -1,22 +1,22 @@
 #! /usr/bin/env ruby
 
 require 'yaml'
+require 'zlib'
 
 scores = {}
-File.open(File.join(__dir__, '../frequency/scores.txt')) do |io|
+
+File.open(File.join(__dir__, '../dictionaries/frequency/scores.txt')) do |io|
   io.each_line do |line|
     line.chomp!
-    (pos, score, freq, word) = line.split(/\t/)
-    unless scores[word]
-      scores[word] = pos.to_i
-    end
+    (pos, word) = line.split(/\t/)
+    scores[word] = pos.to_i
   end
 end
 
 counts = {}
-count_file = File.join(__dir__, 'count.txt')
+count_file = File.join(__dir__, 'count.txt.gz')
 
-File.open(count_file) do |io|
+Zlib::GzipReader.open(count_file) do |io|
   io.each_line.with_index do |line, i|
     line.chomp!
     (pat, count) = line.split(/\t/)
@@ -24,7 +24,8 @@ File.open(count_file) do |io|
   end
 end
 
-words = File.read(File.join(__dir__, '../dicts/web2'))
+words = File.read(File.join(__dir__, '../dictionaries/scowl/en_US-large.txt'))
+words.gsub!(/^.*'s\n/, '')
 
 counts_len = counts.length
 
@@ -36,39 +37,49 @@ counts.each.with_index do |(pat, count), i|
     puts "#{i}/#{counts_len}"
   end
 
-  pat_rex = pat.split(/\*/)
-  pat_rex = /^#{pat_rex[0]}.*#{pat_rex[1]}$/
+  pat_rex = pat.split(/\_/)
+  pat_rex = /^#{pat_rex[0]}.*#{pat_rex[1]}$/i
 
   match_words = words.scan(pat_rex)
 
   word_scores = {}
   total_score = 0
   scored_words = {}
+  hi_scored_words = {}
   not_scored_words = []
+
   match_words.each do |w|
     sc = scores[w] || 0
     word_scores[w] = sc
     total_score += sc
-    if sc > 0
-      scored_words[w] = sc
-    else
+
+    if sc == 0
       not_scored_words << w
+    elsif sc <= 100_000
+      hi_scored_words[w] = sc
+    else
+      scored_words[w] = sc
     end
   end
 
-  out_file = pat.sub(/\*/, '_') + '.yml'
+  out_file = pat + '.yml'
   out_file = File.join(__dir__, 'data', out_file)
 
+  hi_scored_words_count = hi_scored_words.length
   scored_words_count = scored_words.length
 
-  if 8 <= scored_words_count && scored_words_count <= 40
+  if 8 <= hi_scored_words_count && hi_scored_words_count <= 30
     data = {}
     data['pattern'] = pat
+    data['hi_scored_words_count'] = hi_scored_words_count
     data['scored_words_count'] = scored_words_count
     data['total_words'] = match_words.length
     data['score'] = total_score
-    data['score_per_word'] = total_score / scored_words_count
+    if scored_words_count > 0
+      data['score_per_word'] = total_score / (scored_words_count + hi_scored_words_count)
+    end
     data['pickup'] = []
+    data['hi_scored_words'] = hi_scored_words
     data['scored_words'] = scored_words
     data['not_scored_words'] = not_scored_words
 

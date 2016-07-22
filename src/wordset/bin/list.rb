de612b917dc7510b8f380bb4447ecc78cc7a9d85
KOSEKI Kengo <koseki@gmail.com>
2016-07-23 00:33:06 +0900

    Add wordset list

---
+++ src/wordset/bin/list.rb
@@ -0,0 +1,28 @@
+#! /usr/bin/env ruby
+
+
+dir = File.join(__dir__, '../processed/')
+
+files = nil
+map = nil
+Dir.chdir(dir) do
+  lines = %x{wc -l *.yml}
+  map = Hash[lines.split(/\n/).map {|line| line.strip}.map {|line| token = line.split(/\s/); [token[1], token[0]] }]
+end
+
+open(File.join(__dir__, '../list.md'), 'w') do |out|
+  out.puts "# Wordsets"
+  out.puts
+  out.puts "| File           | Entries        |"
+  out.puts "|:---------------|:---------------|"
+
+  map.keys.sort.each do |key|
+    next if key == 'total'
+    if map[key] == '12'
+      out.puts "|#{key.ljust(16)}|#{map[key].ljust(16)}|"
+    else
+      out.puts "|#{key.ljust(16)}|#{"  #{map[key]} *".ljust(16)}|"
+    end
+  end
+end
+
